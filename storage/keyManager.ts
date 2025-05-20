403b8bb485b2b9926400dc03e157bd8d27d878d6
Jonathan Chia Jun Han <jnthnchia@gmail.com>
2025-05-20 09:40:52 -0400

    error regarding importing from keyManager fixed

--- storage/keyManager.ts
+++ storage/keyManager.ts
@@ -1,4 +1,4 @@
-export function generateKey(length: number = 32): string {
+export function generateKey(noteId: string, accessType: string, length: number = 32): string {
     const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     let key = "";
     for (let i = 0; i < length; i++) {
