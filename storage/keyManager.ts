8cb259df634947eca74ebee0db7b4a649290b430
Jonathan Chia Jun Han <jnthnchia@gmail.com>
2025-05-20 09:31:56 -0400

    keyManager created

---
+++ storage/keyManager.ts
@@ -0,0 +1,9 @@
+export function generateKey(length: number = 32): string {
+    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let key = "";
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * charset.length);
+      key += charset[randomIndex];
+    }
+    return key;
+  }
