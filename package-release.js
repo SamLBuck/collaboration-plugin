1f5c82a296a7d393e7c5a7e4fde3229cc962ec06
Samuel <samuellorenbuck@gmail.com>
2025-06-27 08:42:04 -0400

    prepare v1.1.1 for release update

---
+++ package-release.js
@@ -0,0 +1,23 @@
+// package-release.js
+const fs      = require("fs");
+const archiver= require("archiver");
+
+const manifest= JSON.parse(fs.readFileSync("manifest.json","utf8"));
+const zipName = `collaboration-plugin-v${manifest.version}.zip`;
+
+const output  = fs.createWriteStream(zipName);
+const archive = archiver("zip",{ zlib:{ level:9 } });
+
+output.on("close", ()=> console.log(`✔ ${zipName} (${archive.pointer()} bytes)`));
+archive.on("error", err=>{ throw err; });
+
+archive.pipe(output);
+
+// root plugin files
+["manifest.json","main.js","styles.css","data.json"]
+  .forEach(f=> archive.file(f,{ name: f }));
+
+// include the entire dist folder only
+archive.directory("networking/socket/dist", "networking/socket/dist");
+
+archive.finalize();
